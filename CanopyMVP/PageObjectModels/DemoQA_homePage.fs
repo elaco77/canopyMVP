4a609c85196c5ad7d99ab82fadcadcd83475a683
Alen Stojanovic <alen.stojanovic@typeqast.com>
2018-02-15 13:59:27 +0100

    Add homePage and Program

---
+++ CanopyMVP/PageObjectModels/DemoQA_homePage.fs
@@ -0,0 +1,10 @@
+﻿namespace PageObjects
+
+module DemoQA_homePage =
+    open canopy
+
+    let openHomePage() = 
+        url "http://demoqa.com"
+
+    let clickRegistrationButton() =
+        click "#menu-item-374"
---
+++ CanopyMVP/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open canopy
+open PageObjects.DemoQA_homePage
+
+start chrome
+
+"TEST: Register new user on demoQA" &&& fun _ ->
+    PageObjects.DemoQA_homePage.openHomePage()
+    PageObjects.DemoQA_homePage.clickRegistrationButton()
+
+//run all tests
+run()
+
+printfn "press [enter] to exit"
+System.Console.ReadLine() |> ignore
+
+quit()
